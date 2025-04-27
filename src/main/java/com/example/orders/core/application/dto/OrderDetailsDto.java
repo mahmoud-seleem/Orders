12b2b7cb9f73855698570322047e4d6c56e2903d
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-04-27 15:58:52 +0300

    creating the Dtos

---
+++ src/main/java/com/example/orders/core/application/dto/OrderDetailsDto.java
@@ -0,0 +1,53 @@
+package com.example.orders.core.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Date;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)  // Excludes null values
+public class OrderDetailsDto {
+    private Long orderId;
+    private List<ProductDetailsDto> productsDetails;
+    private Date orderDate;
+    private Double totalPrice;
+
+    public OrderDetailsDto(Long orderId, List<ProductDetailsDto> productsDetails, Date orderDate, Double totalPrice) {
+        this.orderId = orderId;
+        this.productsDetails = productsDetails;
+        this.orderDate = orderDate;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<ProductDetailsDto> getProductsDetails() {
+        return productsDetails;
+    }
+
+    public void setProductsDetails(List<ProductDetailsDto> productsDetails) {
+        this.productsDetails = productsDetails;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
---
+++ src/main/java/com/example/orders/core/application/dto/ProductDetailsDto.java
@@ -0,0 +1,50 @@
+package com.example.orders.core.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)  // Excludes null values
+public class ProductDetailsDto {
+    private String productName;
+    private Long productId;
+    private Integer quantity;
+    private Double priceOfUnit;
+
+    public ProductDetailsDto(String productName, Long productId, Integer quantity, Double priceOfUnit) {
+        this.productName = productName;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.priceOfUnit = priceOfUnit;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPriceOfUnit() {
+        return priceOfUnit;
+    }
+
+    public void setPriceOfUnit(Double priceOfUnit) {
+        this.priceOfUnit = priceOfUnit;
+    }
+}
