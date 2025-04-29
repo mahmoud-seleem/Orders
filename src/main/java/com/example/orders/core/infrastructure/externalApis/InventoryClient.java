12de722cbb96d713858e2dce778eb4c5f890f417
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-04-29 13:02:12 +0300

    integrate with eureka service registry

--- src/main/java/com/example/orders/core/infrastructure/externalApis/InventoryClient.java
+++ src/main/java/com/example/orders/core/infrastructure/externalApis/InventoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8080/api/v1/inventory")  // Use the service name registered in Eureka
+@FeignClient(name = "Inventory", path = "api/v1/inventory")  // Use the service name registered in Eureka
 public interface InventoryClient {
 
     @GetMapping("/productExist")
